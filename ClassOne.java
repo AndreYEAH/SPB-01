e5f9af10854961b958b6d9efead02110a2b0f741
AndreYEAH <33290516+AndreYEAH@users.noreply.github.com>
2017-11-19 16:30:57 +0400

    Add files via upload

--- ClassOne.java
+++ ClassOne.java
@@ -4,6 +4,7 @@ public class ClassOne {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Metallica - Hardwired".toUpperCase());
 		System.out.println("Metallica - Spit Out The Bone".toUpperCase());
 		
 		
