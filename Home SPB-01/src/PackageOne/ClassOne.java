a1717990c4761aabbfdaddd2bd141f7d7c970b2a
Андрюха <@192.168.1.27>
2017-11-19 14:06:05 +0300

    111

--- Home SPB-01/src/PackageOne/ClassOne.java
+++ Home SPB-01/src/PackageOne/ClassOne.java
@@ -4,8 +4,8 @@ public class ClassOne {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Metallica - Hardwired".toUpperCase());
 		System.out.println("Metallica - Spit Out The Bone".toUpperCase());
-		
 		
 		
 	}
