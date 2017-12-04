75c0b09660acfeae59250c6c1efd84b80fe7de07
Андрюха <@192.168.1.27>
2017-12-04 11:45:06 +0300

    123

--- Home SPB-01/src/PackageOne/ClassOne.java
+++ Home SPB-01/src/PackageOne/ClassOne.java
@@ -1,13 +1,23 @@
 package PackageOne;
 
+import java.util.Arrays;
+
 public class ClassOne {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Metallica - Hardwired".toUpperCase());
-		System.out.println("Metallica - Spit Out The Bone".toUpperCase());
-		
-		
-		
+
+		if (args == null) {
+			System.out.println("Zero");
+			System.exit(1);
+		}
+
+		if (args.length == 0) {
+			System.out.println("Fuck Yeah");
+			System.exit(0);
+		}
+
+		String argsList = Arrays.toString(args);
+		System.out.println(argsList);
+
 	}
 }
